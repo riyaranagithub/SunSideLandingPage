445a9cacf265d433fe4d0213913305bd08c2e7a3
riyaranagithub <riyarana20062021@gmail.com>
2025-01-11 21:51:23 +0530

    hero section done

--- app/components/hero/HeroSection.tsx
+++ app/components/hero/HeroSection.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './Hero.module.css'
+// import Image from '../../../sunnyside-agency-landing-page-main/images/desktop/image-header.jpg'
+
 function HeroSection() {
   return (
     <>
-    <main className={`mint ${styles.heroheight}`}></main>
+    <div className={styles.herotext}>
+      <h1>WE ARE CREATIVES</h1>
+    </div>
+    <div className={styles.heroarrow}>
+      <img src="/sunnyside-agency-landing-page-main/images/icon-arrow-down.svg" alt="arrow" />
+    </div>
+    <img src="/sunnyside-agency-landing-page-main/images/desktop/image-header.jpg" alt="heroimage" className={styles.heroimage}/>
     </>
   )
 }
